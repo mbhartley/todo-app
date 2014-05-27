fa06ae9c802fe6bfbdf08e1e2a7c1fa807cd1de0
Matt Hartley <mbhartley1490@gmail.com>
2014-05-27 12:07:02 -0400

    final cleanup of app functionality

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -22,31 +22,94 @@ $(document).ready(function(){
 
   //creates a reference to  our template in html
   var todoTemplate = _.template($('.todo-template').text())	
+
+  //create an empty array
+  todoArray = [];
   
   //render pre-loaded data onto page
   _.each(todoList, function(item){					
     $('.todo-items').prepend(todoTemplate(item))
   })
 
-  //grab the input value on click and store it in a var description
-  $('.add-button').click(function(){					
-    var description = $('.js-tasks-input').val();	    
+  // ON CLICK ADD BUTTON
 
-    var todo = {										//create object literal with the var description ???
-      description: description,						
-      done: false,									//done: false (so, later we can set done button up)
-      id: _.uniqueId()							//give a unique id for the purpose of storing it in an array
+  /*grab the input value and store it in a var description,
+  then clear out the input field automatically*/
+
+  $('.add-button').click(function(){	
+
+    var description = $('.js-tasks-input').val();	
+    $('.js-tasks-input').val('');    
+
+    var todo = {										
+      description: description || 'no item added',						
+      done: false,									
+      id: _.uniqueId('todo')							
     }
 
-    //store the rendered template string ???
+    //store the rendered template string
+
     var renderedTemplate = todoTemplate(todo);	    
-  
+    
     //prepend the rendered template into todo-items div
+
     $('.todo-items').prepend(renderedTemplate);			
+
+    todoArray.push(todo);
  
+  });
 
-  })
+});
 
-})
+// ON CLICK DELETE BUTTON
+
+$('.todo-items').on('click', '.js-remove-todo', function(){
+
+    var parentId = $(this).parent().attr('id');
+
+    todoArray = _.reject(todoArray, function(item){
+      return item.id == parentId;
+    });
+
+    $(this).parent().remove();
+
+  });
+
+// ON CLICK EDIT BUTTON
+
+$('.todo-items').on('click', '.edit-button', function(){
+  console.log("EDIT");
+    $(this).siblings('.edit-input').show();
+
+    $(this).siblings('.edit-input').focus();
+
+  });
+
+  $('.todo-items').on('blur', '.edit-input', function(){
+      var parentId = $(this).parent().attr('id');
+      var newDescription = $(this).val();
+    var item = _.findWhere(todoArray, {id: parentId});
+
+    item.description = newDescription 
+
+    $(this).siblings('.description').empty().html(newDescription)
+    $(this).hide()
+    
+  });
+
+// ON CLICK COMPLETE BUTTON
+
+$('.todo-items').on('click', '.complete-button', function(){
+  console.log('STRIKE ONE');
+    $(this).siblings('.description').toggleClass('strikethrough');
+
+
+  });
+
+
+
+
+
+
 
 
